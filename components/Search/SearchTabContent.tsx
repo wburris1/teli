fd4d442e386e41b43b1d646c9c5765165012c636
adichak27 <adichak2002@gmail.com>
2024-09-04 09:15:04 -0400

    switch  between grid and list

--- components/Search/SearchTabContent.tsx
+++ components/Search/SearchTabContent.tsx
@@ -1,18 +1,20 @@
 import { useCallback, useEffect, useState } from "react";
 import ItemScreen from "./SearchCard";
 import { useItemSearch } from "@/data/itemData";
-import _ from 'lodash';
+import _, { set } from 'lodash';
 import { RenderGrid } from "../GridItems";
 
 export const MoviesTabContent = ({ query, isAdding, addItems, outItems, setAddItems, setOutItems, listID }:
     { query: string, isAdding: boolean, addItems: Item[], outItems: Item[], 
     setAddItems: (items: Item[]) => void, setOutItems: (items: Item[]) => void, listID: string }) => {
     const [movieList, setMovieList] = useState<Item[]>([]);
+    const [displayGrid, setDisplayGrid] = useState(true);
 
     const debouncedFetchData = useCallback(
         _.debounce(async (query) => {
             const items = await useItemSearch(query, true);
             setMovieList(items);
+            setDisplayGrid(query ? false : true);
         }, 300),
         []
     );
@@ -20,20 +22,24 @@ export const MoviesTabContent = ({ query, isAdding, addItems, outItems, setAddIt
     useEffect(() => {
         debouncedFetchData(query);
     }, [query, debouncedFetchData]);
-    // <ItemScreen movieList={movieList} isAdding={isAdding} addItems={addItems} outItems={outItems}
-    // setAddItems={setAddItems} setOutItems={setOutItems} listID={listID} />
-    return <RenderGrid listID={listID} items={movieList} />
+    return (
+      displayGrid ? <RenderGrid listID={listID} items={movieList} /> 
+      : (<ItemScreen movieList={movieList} isAdding={isAdding} addItems={addItems} outItems={outItems}
+         setAddItems={setAddItems} setOutItems={setOutItems} listID={listID} /> )
+  )
 };
 
 export const ShowsTabContent = ({ query, isAdding, addItems, outItems, setAddItems, setOutItems, listID }:
     { query: string, isAdding: boolean, addItems: Item[], outItems: Item[], 
     setAddItems: (items: Item[]) => void, setOutItems: (items: Item[]) => void, listID: string }) => {
     const [tvList, setTvList] = useState<Item[]>([]);
+    const [displayGrid, setDisplayGrid] = useState(true);
     
     const debouncedFetchData = useCallback(
         _.debounce(async (query) => {
             const items = await useItemSearch(query, false);
             setTvList(items);
+            setDisplayGrid(query ? false : true);
         }, 300),
         []
     );
@@ -42,8 +48,9 @@ export const ShowsTabContent = ({ query, isAdding, addItems, outItems, setAddIte
         debouncedFetchData(query);
     }, [query, debouncedFetchData]);
 
-    return <RenderGrid listID={listID} items={tvList} />
-
-    // return <ItemScreen movieList={tvList} isAdding={isAdding} addItems={addItems} outItems={outItems}
-    // setAddItems={setAddItems} setOutItems={setOutItems} listID={listID} />;
+    return (
+      displayGrid ? <RenderGrid listID={listID} items={tvList} />
+      : (<ItemScreen movieList={tvList} isAdding={isAdding} addItems={addItems} outItems={outItems}
+        setAddItems={setAddItems} setOutItems={setOutItems} listID={listID} /> )
+    )
 };
