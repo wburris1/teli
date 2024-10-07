a54ed6c9929ce960d0e17500d470c4c8a7ef4eac
adichak27 <adichak2002@gmail.com>
2024-10-06 21:31:05 -0400

    default casst members

--- components/CastList.tsx
+++ components/CastList.tsx
@@ -3,18 +3,25 @@ import { CastMember } from "@/constants/ImportTypes"
 import { TouchableOpacity, StyleSheet, Image, useColorScheme } from "react-native"
 import { Text, View } from "./Themed"
 import Colors from "@/constants/Colors"
+import { DefaultPost } from "./LogoView"
 
 
 const screenWidth = Dimensions.screenWidth
 const imgUrl = 'https://image.tmdb.org/t/p/w342';
 const itemWidth = (Dimensions.screenWidth / 3) - 20;
+const actorFontSize = screenWidth > 400 ? 12 : 8;
 
 export const CastList =  ({ cast }: { cast: CastMember }) => {
   const colorScheme = useColorScheme();
 
   return (
     <View style={styles.itemContainer} >
-      <Image source={{ uri: imgUrl + cast.profile_path }} style={[styles.Itemimage, {borderColor: Colors[colorScheme ?? 'light'].text}]} />
+      {cast.profile_path ? 
+      <Image
+                                source={{ uri: imgUrl + cast.profile_path }}
+                                style={[styles.Itemimage, {borderColor: Colors[colorScheme ?? 'light'].text}]}
+                                /> : <DefaultPost text='Cast Member Not Available' fontSize={actorFontSize} style={[styles.Itemimage, {borderColor: Colors[colorScheme ?? 'light'].text, overflow: 'hidden'}]}/>}
+
       <Text numberOfLines={1} ellipsizeMode="tail" style={styles.actorName}>
         {cast.name}
       </Text>
@@ -52,7 +59,7 @@ const styles = StyleSheet.create({
     },
     characterName: {
       marginTop: 2,
-      fontSize: screenWidth > 400 ? 12 : 8, //8, //12
+      fontSize: actorFontSize,
       fontWeight: '400',
       textAlign: 'center',
       width: itemWidth - 20,
